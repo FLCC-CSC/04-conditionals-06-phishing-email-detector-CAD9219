86164e356ff8cb2c5f85620f87be0e8bf6b53579
Collin Dillabough <cdillabough@fingerlakes.edu>
2026-02-27 18:42:10 +0000

    Fixed typos and formatting

--- phishing_email_detector.py
+++ phishing_email_detector.py
@@ -1,8 +1,8 @@
 # FILE NAME - phishing_email_detector.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Collin Dillabough
+# DATE:02/27/2026 
+# BRIEF DESCRIPTION: Program for a phishing mail detector.  
 
 
 
@@ -17,20 +17,23 @@
 
 
 subject_line = input('Enter the email subject line:')
+print('SECURITY ASSESSMENT:')
 
-if 'urgent' or 'immediate action required' in subject_line.lower:
+subject_lower = subject_line.lower()
+
+if 'urgent' in subject_lower or 'immediate action required' in subject_lower:
     print('HIGH RISK: Possible phishing attempt.')
 
-elif 'win' or 'free' in subject_line.lower:
+elif 'win' in subject_lower or 'free' in subject_lower:
     print('MEDIUM RISK: Suspicious offer detected.')
 
-elif 'password reset' in subject_line.lower:
+elif 'password reset' in subject_lower:
     print('LOW RISK: Verify legitimacy with sender.') 
 
 else:
     print('No phishing indicators detected.')
 
-print('Analyzed subject:', '"'+subject_line+'"')
+print('------------------------ \nAnalyzed subject:', '"'+subject_lower+'"')
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
@@ -86,7 +89,8 @@ Analyzed subject: "Did you request a password reset?"
 '''
 
 1. Was using `in` difficult or was it natural?
-
+it was natural for me. I struggled with using the .lower. I was trying to alter the variable in the if statement. but I like the use of 'in' and 'or'.
+I understand the logic of the statements its just the syntax. I didn't use AI to write the code but I did use the explain my mistake feature in the autograde details to catch a typo. 
 
 
 
@@ -107,7 +111,7 @@ Please gauge your utilization of AI on the following spectrum. Place an "X" in f
 of the appropriate response. Only choose one of the following:
 
 [ ] I did not use AI at all for this lab.
-[ ] I wrote the initial draft of the software but had AI help me make it better.
+[x] I wrote the initial draft of the software but had AI help me make it better.
 [ ] I fed the lab description to AI and had it generate a response but I modified it.
 [ ] AI created the entire program for me.
 
@@ -121,6 +125,6 @@ may impede your understanding. Please rate how well you understand the concepts 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
 [ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
+[x] I'm solid. Totally got it.
 
 '''
